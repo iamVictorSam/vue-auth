2004acd9badaf1c5f5b4eaf9cede905814b5c3f6
Sam Victor <iamvictorsam@gmail.com>
2023-08-13 15:45:44 +0100

    init

--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import OktaVue from '@okta/okta-vue'
 import App from './App.vue'
 import router from './router'
 
-
 const oktaAuth = new OktaAuth({
   clientId: '0oa5uopvzs8AgxjfI5d7',
   issuer: 'https://dev-23240032.okta.com/oauth2/default',
   redirectUri: `${window.location.origin}/login/callback`,
   scopes: ['openid', 'profile', 'email']
-})
-
-
-
+});
 
 createApp(App)
   .use(router)
